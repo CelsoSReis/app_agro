2f78251b21f76b4a457150dc569fd7d0ae5c5c45
Celso Reis <87393416+CelsoSReis@users.noreply.github.com>
2022-08-26 21:35:04 -0300

    Criando Tela
    Criando Tela

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View} from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.img}/>
+      <Text style={styles.texto}>Gestão Rural!</Text>
+      <Text style={styles.rodape}>Desenvolvido por Celso Reis</Text>
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#006400',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontSize: 25,
+    marginBottom: 200,
+    color: '#fff'
+  },
+  img:{
+    width: 210,
+    height: 65,
+    marginBottom: 200
+  },
+  rodape:{
+    color: '#fff',
+  }
 });
